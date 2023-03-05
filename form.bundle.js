3bae92785cec4144f0c06c8d3caa4af645c7f19f
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-03-05 03:05:54 -0800

    Updates

---
+++ form.bundle.js
@@ -0,0 +1,38 @@
+"use strict";
+(self["webpackChunkweather_app"] = self["webpackChunkweather_app"] || []).push([["form"],{
+
+/***/ "./src/form.js":
+/*!*********************!*\
+  !*** ./src/form.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "buildForm": () => (/* binding */ buildForm)
+/* harmony export */ });
+const buildForm = () => {
+  const form = document.createElement("form");
+  const input = document.createElement("input");
+  const submitBtn = document.createElement("button");
+  const main = document.querySelector("main");
+
+  submitBtn.innerHTML = "Search";
+
+  form.appendChild(input);
+  form.appendChild(submitBtn);
+  main.appendChild(form);
+};
+
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/form.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZm9ybS5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7O0FBRUE7O0FBRUE7QUFDQTtBQUNBO0FBQ0E7O0FBRXFCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VhdGhlci1hcHAvLi9zcmMvZm9ybS5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBidWlsZEZvcm0gPSAoKSA9PiB7XG4gIGNvbnN0IGZvcm0gPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KFwiZm9ybVwiKTtcbiAgY29uc3QgaW5wdXQgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KFwiaW5wdXRcIik7XG4gIGNvbnN0IHN1Ym1pdEJ0biA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoXCJidXR0b25cIik7XG4gIGNvbnN0IG1haW4gPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKFwibWFpblwiKTtcblxuICBzdWJtaXRCdG4uaW5uZXJIVE1MID0gXCJTZWFyY2hcIjtcblxuICBmb3JtLmFwcGVuZENoaWxkKGlucHV0KTtcbiAgZm9ybS5hcHBlbmRDaGlsZChzdWJtaXRCdG4pO1xuICBtYWluLmFwcGVuZENoaWxkKGZvcm0pO1xufTtcblxuZXhwb3J0IHsgYnVpbGRGb3JtIH07XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
